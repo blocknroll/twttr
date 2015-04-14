9fe7a876a885da4be4f80ef6200b1f071fd37003
Fred Block <info@fredblock.com>
2015-04-14 11:27:36 -0600

    Add test for invalid login in 'users_login_test.rb'. Update create method in 'sessions_controller.rb'.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in user and redirect to user's show page
+    else
+      # create an error message
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
