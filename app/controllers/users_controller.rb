bc2ca923c26db16c989a19f412c2c94bc0e8b5dd
Fred Block <info@fredblock.com>
2015-05-14 08:58:16 -0600

    Add users index page and initial test.
    Update 'users_controller_test.rb' 'users_controller.rb' 'users_helper.rb' 'users/index.html.erb' '_header.html.erb' and 'custom.css.scss'.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def show
@@ -33,6 +37,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
 
 
     private
