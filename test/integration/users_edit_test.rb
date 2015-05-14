837d0f10d3db4ed02700b463f66b5d62d396d71c
Fred Block <info@fredblock.com>
2015-05-13 18:14:39 -0600

    Add friendly forwarding.
    Add integration test in 'users_edit_test.rb'. Add 'store_location' and 'redirect_back_or' methods in 'sessions_helper.rb'. Update 'sessions_controller.rb' and 'users_controller.rb'.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,10 +17,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name,
@@ -30,7 +30,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
-    assert_equal @user.name, name
-    assert_equal @user.email, email
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+    # assert_equal @user.name, name
+    # assert_equal @user.email, email
   end
+
+
 end
