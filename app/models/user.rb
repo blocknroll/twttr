16e7e32d82bf28df01deddfa0494148415850fa4
Fred Block <info@fredblock.com>
2015-05-15 13:33:12 -0600

    Refactor user account activations. Push logic down into user model.
    Update 'users_controller.rb' 'user.rb' and 'account_activations_controller.rb'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,22 @@ class User < ActiveRecord::Base
   end
 
 
+  # Activates an account
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
+
+  # Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+
+
+
+      # Private //////////////////////////////////////////////////////
 
       private
 
